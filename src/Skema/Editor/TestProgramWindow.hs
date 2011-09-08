a0e2cacb24bff056f6334ce4effa9b7240b83bbd
Luis Cabellos <cabellos@ifca.unican.es>
2011-09-08 13:04:21 +0200

    change double to DataValue

--- src/Skema/Editor/TestProgramWindow.hs
+++ src/Skema/Editor/TestProgramWindow.hs
@@ -39,10 +39,12 @@ import Graphics.UI.Gtk.Entry.SpinButton(
 import Graphics.UI.Gtk.Misc.Adjustment( adjustmentNew )
 import Graphics.UI.Gtk.Ornaments.HSeparator( hSeparatorNew )
 import System.Glib.Signals( on )
-import Skema.Types( IOPointDataType(..), IOPointType(..) )
+import Skema.Types( 
+  IOPointDataType(..), IOPointType(..), dataTypeBase, dataTypeVectorSize )
 import Skema.ProgramFlow( 
   ProgramFlow, IOPoint(..), unasignedOutputPoints, unasignedInputPoints )
 import Skema.SIDMap( toInt )
+import Skema.DataValue( DataValue(..), updateDataValue )
 
 -- -----------------------------------------------------------------------------
 showTestProgramWindow :: ProgramFlow -> IO ()
@@ -103,7 +105,7 @@ showTestProgramWindow pf = do
   return ()
 
 -- -----------------------------------------------------------------------------
-createIOControl :: BoxClass b => b -> IOPoint -> IO [(MVar Double,SpinButton)]
+createIOControl :: BoxClass b => b -> IOPoint -> IO [(MVar DataValue,SpinButton)]
 createIOControl box pt = do
   hbox0 <- hBoxNew True 0
   lblName <- labelNew . Just $ 
@@ -123,153 +125,46 @@ createIOControl box pt = do
 bounds :: Bounded a => (a,a)
 bounds = (minBound, maxBound)
 
+configureEntry :: (BoxClass b, Num n) => b -> (n -> DataValue)
+                  -> Double -> Double -> Double -> Int
+                  -> IO [(MVar DataValue,SpinButton)]
+configureEntry box f ma mb step decimals = do
+  adjust <- adjustmentNew 0 ma mb step step 0
+  entry <- spinButtonNew adjust step decimals
+  dat <- newMVar $ f 0
+  _ <- afterInput entry $ do
+    newval <- spinButtonGetValue entry
+    modifyMVar_ dat (return . updateDataValue newval)
+    return $ Just newval  
+  boxPackStart box entry PackNatural 0
+  return [(dat, entry)]
+  
+configureIntEntry :: (BoxClass b, Num n, Integral d) 
+                     => b -> (n -> DataValue) -> (d,d)
+                     -> IO [(MVar DataValue,SpinButton)]
+configureIntEntry box f (ma, mb) = 
+  configureEntry box f (fromIntegral ma) (fromIntegral mb) 1 0
+  
 createEntry :: BoxClass b => b -> IOPointDataType 
-               -> IO [(MVar Double,SpinButton)]
-createEntry box IOchar = do
-  adjust <- adjustmentNew 0 (fromIntegral ma) (fromIntegral mb) 1 1 0
-  entry <- spinButtonNew adjust 1 0
-  boxPackStart box entry PackNatural 0
-  dat <- newMVar 0
-  _ <- afterInput entry $ do
-    newval <- spinButtonGetValue entry
-    modifyMVar_ dat (const $ return newval)
-    return $ Just newval  
-  return [(dat, entry)]
-    where
-      (ma,mb) = bounds :: (Int8, Int8)
-createEntry box IOuchar = do
-  adjust <- adjustmentNew 0 (fromIntegral ma) (fromIntegral mb) 1 1 0
-  entry <- spinButtonNew adjust 1 0
-  boxPackStart box entry PackNatural 0
-  dat <- newMVar 0
-  _ <- afterInput entry $ do
-    newval <- spinButtonGetValue entry
-    modifyMVar_ dat (const $ return newval)
-    return $ Just newval  
-  return [(dat, entry)]
-    where
-      (ma,mb) = bounds :: (Word8, Word8)
-createEntry box IOshort = do
-  adjust <- adjustmentNew 0 (fromIntegral ma) (fromIntegral mb) 1 1 0
-  entry <- spinButtonNew adjust 1 0
-  boxPackStart box entry PackNatural 0
-  dat <- newMVar 0
-  _ <- afterInput entry $ do
-    newval <- spinButtonGetValue entry
-    modifyMVar_ dat (const $ return newval)
-    return $ Just newval  
-  return [(dat, entry)]
-    where
-      (ma,mb) = bounds :: (Int16, Int16)
-createEntry box IOushort = do
-  adjust <- adjustmentNew 0 (fromIntegral ma) (fromIntegral mb) 1 1 0
-  entry <- spinButtonNew adjust 1 0
-  boxPackStart box entry PackNatural 0
-  dat <- newMVar 0
-  _ <- afterInput entry $ do
-    newval <- spinButtonGetValue entry
-    modifyMVar_ dat (const $ return newval)
-    return $ Just newval  
-  return [(dat, entry)]
-    where
-      (ma,mb) = bounds :: (Word16, Word16)
-createEntry box IOint = do
-  adjust <- adjustmentNew 0 (fromIntegral ma) (fromIntegral mb) 1 1 0
-  entry <- spinButtonNew adjust 1 0
-  boxPackStart box entry PackNatural 0
-  dat <- newMVar 0
-  _ <- afterInput entry $ do
-    newval <- spinButtonGetValue entry
-    modifyMVar_ dat (const $ return newval)
-    return $ Just newval  
-  return [(dat, entry)]
-    where
-      (ma,mb) = bounds :: (Int32, Int32)
-createEntry box IOuint = do
-  adjust <- adjustmentNew 0 (fromIntegral ma) (fromIntegral mb) 1 1 0
-  entry <- spinButtonNew adjust 1 0
-  boxPackStart box entry PackNatural 0
-  dat <- newMVar 0
-  return [(dat, entry)]
-    where
-      (ma,mb) = bounds :: (Word32, Word32)
-createEntry box IOlong = do
-  adjust <- adjustmentNew 0 (fromIntegral ma) (fromIntegral mb) 1 1 0
-  entry <- spinButtonNew adjust 1 0
-  boxPackStart box entry PackNatural 0
-  dat <- newMVar 0
-  _ <- afterInput entry $ do
-    newval <- spinButtonGetValue entry
-    modifyMVar_ dat (const $ return newval)
-    return $ Just newval  
-  return [(dat, entry)]
-    where
-      (ma,mb) = bounds :: (Int64, Int64)
-createEntry box IOulong = do
-  adjust <- adjustmentNew 0 (fromIntegral ma) (fromIntegral mb) 1 1 0
-  entry <- spinButtonNew adjust 1 0
-  boxPackStart box entry PackNatural 0
-  dat <- newMVar 0
-  _ <- afterInput entry $ do
-    newval <- spinButtonGetValue entry
-    modifyMVar_ dat (const $ return newval)
-    return $ Just newval  
-  return [(dat, entry)]
-    where
-      (ma,mb) = bounds :: (Word64, Word64)
-createEntry box IOfloat = do
-  adjust <- adjustmentNew 0 (-1e50) 1e50 0.01 0.01 0
-  entry <- spinButtonNew adjust 0.01 2
-  boxPackStart box entry PackNatural 0
-  dat <- newMVar 0
-  _ <- afterInput entry $ do
-    newval <- spinButtonGetValue entry
-    modifyMVar_ dat (const $ return newval)
-    return $ Just newval  
-  return [(dat, entry)]
-createEntry box IOchar2 = createNEntries box IOchar 2
-createEntry box IOchar4 = createNEntries box IOchar 4
-createEntry box IOchar8 = createNEntries box IOchar 8
-createEntry box IOchar16 = createNEntries box IOchar 16
-createEntry box IOuchar2 = createNEntries box IOuchar 2
-createEntry box IOuchar4 = createNEntries box IOuchar 4
-createEntry box IOuchar8 = createNEntries box IOuchar 8
-createEntry box IOuchar16 = createNEntries box IOuchar 16
-createEntry box IOshort2 = createNEntries box IOshort 2
-createEntry box IOshort4 = createNEntries box IOshort 4
-createEntry box IOshort8 = createNEntries box IOshort 8
-createEntry box IOshort16 = createNEntries box IOshort 16
-createEntry box IOushort2 = createNEntries box IOushort 2
-createEntry box IOushort4 = createNEntries box IOushort 4
-createEntry box IOushort8 = createNEntries box IOushort 8
-createEntry box IOushort16 = createNEntries box IOushort 16
-createEntry box IOint2 = createNEntries box IOint 2
-createEntry box IOint4 = createNEntries box IOint 4
-createEntry box IOint8 = createNEntries box IOint 8
-createEntry box IOint16 = createNEntries box IOint 16
-createEntry box IOuint2 = createNEntries box IOuint 2
-createEntry box IOuint4 = createNEntries box IOuint 4
-createEntry box IOuint8 = createNEntries box IOuint 8
-createEntry box IOuint16 = createNEntries box IOuint 16
-createEntry box IOlong2 = createNEntries box IOlong 2
-createEntry box IOlong4 = createNEntries box IOlong 4
-createEntry box IOlong8 = createNEntries box IOlong 8
-createEntry box IOlong16 = createNEntries box IOlong 16
-createEntry box IOulong2 = createNEntries box IOulong 2
-createEntry box IOulong4 = createNEntries box IOulong 4
-createEntry box IOulong8 = createNEntries box IOulong 8
-createEntry box IOulong16 = createNEntries box IOulong 16
-createEntry box IOfloat2 = createNEntries box IOfloat 2
-createEntry box IOfloat4 = createNEntries box IOfloat 4
-createEntry box IOfloat8 = createNEntries box IOfloat 8
-createEntry box IOfloat16 = createNEntries box IOfloat 16
+               -> IO [(MVar DataValue,SpinButton)]
+createEntry b IOchar = configureIntEntry b DVchar (bounds::(Int8,Int8))
+createEntry b IOuchar = configureIntEntry b DVuchar (bounds::(Word8,Word8))
+createEntry b IOshort = configureIntEntry b DVshort (bounds::(Int16,Int16))
+createEntry b IOushort = configureIntEntry b DVushort (bounds::(Word16,Word16))
+createEntry b IOint = configureIntEntry b DVint (bounds::(Int32,Int32))
+createEntry b IOuint = configureIntEntry b DVuint (bounds::(Word32,Word32))
+createEntry b IOlong = configureIntEntry b DVlong (bounds::(Int64,Int64))
+createEntry b IOulong = configureIntEntry b DVulong (bounds::(Word64,Word64))
+createEntry b IOfloat = configureEntry b DVfloat (-1e50) 1e50 0.01 2
+
+createEntry b t = createNEntries b (dataTypeBase t) (dataTypeVectorSize t)
 
 createNEntries :: BoxClass b => b -> IOPointDataType -> Int 
-                  -> IO [(MVar Double,SpinButton)]
-createNEntries box t n = do
+                  -> IO [(MVar DataValue,SpinButton)]
+createNEntries b t n = do
   vbox <- vBoxNew True 0
   ret <- forM [0..n-1] $ \_ -> createEntry vbox t
-  boxPackStart box vbox PackNatural 0
+  boxPackStart b vbox PackNatural 0
   return $ concat ret
 
 -- -----------------------------------------------------------------------------
